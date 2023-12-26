1a8ef499d3e5da46e92b729ac060c518002948e0
IrinaAbrosimova <irina.abrosimova@live.com>
2023-12-26 13:04:28 +0300

    D10

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -49,6 +49,12 @@ EMAIL_ADMIN = EMAIL_HOST_USER
 APSCHEDULER_DATETIME_FORMAT = 'N j, Y, F:s a'
 APSCHEDULER_RUN_NOW_TIMEOUT = 25
 
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
